4a6ef89db5840e044430df4981df651ba7b65ab3
David Cortes <dcortes22@gmail.com>
2020-10-23 14:51:08 -0600

    First Commit

---
+++ Sources/AmpDownloader/AmpDownloader.swift
@@ -0,0 +1,3 @@
+struct AmpDownloader {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import AmpDownloaderTests
+
+var tests = [XCTestCaseEntry]()
+tests += AmpDownloaderTests.allTests()
+XCTMain(tests)
